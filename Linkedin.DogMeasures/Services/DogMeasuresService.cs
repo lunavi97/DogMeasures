a089767924e5e0b8ca34df9a63d805d3b4f7dbf6
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-23 23:42:05 -0300

    Added dog measures service with list of dogs and their measures

---
+++ Linkedin.DogMeasures/Services/DogMeasuresService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkedin.DogMeasures.Services
+{
+    public class DogMeasuresService
+    {
+        private readonly List<Models.DogMeasures> _dogInfo;
+    }
+}
