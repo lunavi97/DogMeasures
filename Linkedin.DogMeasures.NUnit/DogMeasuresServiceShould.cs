0e89b2a37afa4021b0db450ce07c16a72e39f72c
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-24 16:53:46 -0300

    Added class as test fixture

---
+++ Linkedin.DogMeasures.NUnit/DogMeasuresServiceShould.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linkedin.DogMeasures.NUnit
+{
+    [TestFixture]
+    public class DogMeasuresServiceShould
+    {
+    }
+}
