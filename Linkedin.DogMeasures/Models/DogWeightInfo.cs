0fa88725060ba0de719f9c00431a4130374917d1
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-23 23:35:31 -0300

    Added Dog Weight Info

---
+++ Linkedin.DogMeasures/Models/DogWeightInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkedin.DogMeasures.Models
+{
+    public class DogWeightInfo
+    {
+        public enum WeightDeviationType
+        {
+            InRange,
+            BelowWeight,
+            Overweight
+        }
+
+        public WeightDeviationType DeviationType { get; set; }
+
+        public string Message { get; set; }
+
+        public decimal WeightDeviation { get; set; }
+    }
+}
