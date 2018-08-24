290f4e6253a8ab3bf06b38c43ca0934604aed0d6
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-24 01:46:59 -0300

    Created test class, added setup with class initialize and linked reference to server

---
+++ Linkedin.DogMeasures.Tests/DogMeasuresServiceShould.cs
@@ -0,0 +1,20 @@
+﻿using Linkedin.DogMeasures.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linkedin.DogMeasures.Tests
+{
+    [TestClass]
+    public class DogMeasuresServiceShould
+    {
+        private static DogMeasuresService _dogMeasuresService;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _dogMeasuresService = new DogMeasuresService();
+        }
+    }
+}
